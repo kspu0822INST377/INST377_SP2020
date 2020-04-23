a0a08964d109718cb6b679dfb188435fa90942c5
Alex Leitch <aleitch1@umd.edu>
2020-04-23 17:34:30 -0400

    Update writeuser.js

--- Lab Problems/lab_10/libraries/writeuser.js
+++ Lab Problems/lab_10/libraries/writeuser.js
@@ -6,9 +6,15 @@ async function writeUser(username, dbSettings) {
     const db = await open(dbSettings)
     await db.exec('CREATE TABLE IF NOT EXISTS user (name)');
     await db.exec(`INSERT INTO user VALUES ("${username}")`);
-    const result = await db.each('SELECT * FROM user');
+    const result = await db.each('SELECT * FROM user', (err) => {
+        console.log('writeuser', err);
+    });
     console.log('Expected result', result);
     return result;
   }
   
-  export default writeUser;
+export default writeUser;
+
+//   import writeUser from "./libraries/writeuser";
+
+// 
