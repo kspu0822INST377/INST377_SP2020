1caa84b483914f7163b804f969fa2e08f6adf6a8
Alex Leitch <aleitch1@umd.edu>
2020-04-23 10:23:26 -0400

    Add sqlite3 sqlite promise library write demo functions

--- Lab Problems/lab_10/server.js
+++ Lab Problems/lab_10/server.js
@@ -1,24 +1,28 @@
 // These are our required libraries to make the server work.
 
-const express = require('express');
-const fetch = require('node-fetch');
-const sqlite3 = require('sqlite3').verbose(); // We're including a server-side version of SQLite, the in-memory SQL server.
-
+import express from 'express';
+import fetch from 'node-fetch';
+// const sqlite3 = require('sqlite3').verbose(); // We're including a server-side version of SQLite, the in-memory SQL server.
+// const open = sqlite3.open; // We're including a server-side version of SQLite, the in-memory SQL server.
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import writeUser from './libraries/writeuser';
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const db = new sqlite3.Database(':memory:', (err) => {
-  if(err){
-    return console.error('err.message');
-  }
-  console.log('Connected to the in-memory SQL database');
-});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// this is new! It's an asynchronous function. We need plugins to use it, and it's not safe for the front end.
+async function databaseBoot() {
+  console.log('async DB boot');
+  const db = await open({
+    filename: '/tmp/database2.db',
+    driver: sqlite3.Database
+  })
+};
 
 
 function processDataForFrontEnd(req, res) {
@@ -43,10 +47,20 @@ function processDataForFrontEnd(req, res) {
 // or we'll end up with spelling errors in our endpoints.
 // 
 app.route('/api')
-  .get((req, res) => {processDataForFrontEnd(req, res)})
+  .get((req, res) => {
+    processDataForFrontEnd(req, res)
+  })
   .post((req, res) => {
     console.log("/api post request", req.body);
+    if(!req.body.name){
+      res.status('418').send('something went wrong, additionally i am a teapot')
+    }
+    writeUser(req.body.name);
     res.send('your request was successful'); // simple mode
   })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+  databaseBoot();
+});
+
